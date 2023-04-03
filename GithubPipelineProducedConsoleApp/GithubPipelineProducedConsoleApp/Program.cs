ee7b47f3a84e68ec6615c257cb558c9a66a60bf2
xdvarpunen <ismail.belmostefa@gmail.com>
2023-04-03 17:15:07 +0300

    Initial commit

---
+++ GithubPipelineProducedConsoleApp/GithubPipelineProducedConsoleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GithubPipelineProducedConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
